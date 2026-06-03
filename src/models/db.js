08b32aba52c4d77dd7fccd42734a2565a813968c
adelso.tzep <atzepchox@byupathway.edu>
2026-06-03 16:37:49 -0600

    Changes applied

--- src/models/db.js
+++ src/models/db.js
@@ -11,10 +11,14 @@ import { Pool } from 'pg';
  * The connection string format is:
  * postgresql://username:password@host:port/database
  */
+
 const pool = new Pool({
-    connectionString: process.env.DB_URL,
-    ssl: true
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
+
 
 /**
  * Common SSL Issue:
